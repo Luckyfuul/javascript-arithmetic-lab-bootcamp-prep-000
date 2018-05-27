40e453931060102dd33cd76dca0906846e00b05c
TJ Zuvic <terrance@zuvmarketing.com>
2018-05-27 06:15:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(x,y){
+  return x + y
+}
+function subtract(x,y){
+  return x - y
+}
+function divide(x,y){
+  return x / y
+}
+function multiply(x,y){
+  return x * y
+}
+function inc(n) {
+  return n+= 1
+}
+function dec(n){
+  return n-= 1
+}
+function makeInt(n){
+  parseInt(n);
+  return n
+}
+function makeInt(n){
+  parseInt(n,10)
+  return n
+}
+parseInt(n,10)
